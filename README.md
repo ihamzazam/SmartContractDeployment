59fb9c47a8b5211af7ede762a520526fe8a6d7bb
Hamza <hamzazam00@gmail.com>
2023-03-08 10:56:15 +0800

    EW to SCD

--- README.md
+++
@@ -1,9 +0,0 @@
-# Smart Contract Deployment
-
-In this repository you can see an example of how a simple smart contract deployment can be automated using GitHub Actions workflow. 
-
-## Dev Env
-
-Hardhat frame work is used for the deployment of the smart contract.
-
-This [Tutorial](https://hardhat.org/tutorial/creating-a-new-hardhat-project) was followed to set up the development environment and create relevant files.
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+
+require('@openzeppelin/hardhat-upgrades');
+
+
+
+
 //require('dotenv').config(); 
+const deployKey = "b7059a1f8aacf38cba35a2075d6df5b52c5f08786c69d80ae307466a7e9f1cbc";
 
-const deployKey = process.env.PRIVATE_KEY;
-
-//const deployKey = '54ee5949d58d0b1e7fa479ffd8cf182b0ec18977278c6f38e26024760d37d8ab'
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-//const ALCHEMY_API_KEY = "QRdWak4iQNV6uH4mh2h66k3ApBj9bWTX";
+const ALCHEMY_API_KEY = "fTj-d_vmXZ_YFh9JfH1ZCrBmd_kH--Vq";
 
 // Replace this private key with your Goerli account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-//const GOERLI_PRIVATE_KEY = "54ee5949d58d0b1e7fa479ffd8cf182b0ec18977278c6f38e26024760d37d8ab";
+const GOERLI_PRIVATE_KEY = "e51acde034d4294589ec534d0c223a1d87e5a50857658e0194364a6b5d746f29";
 
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    /*goerli: {
+    goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
-    },*/
+    },
     volta: {
       url: "https://volta-rpc.energyweb.org",
       accounts: [deployKey],
@@ -32,7 +36,8 @@ module.exports = {
       url: "https://rpc.energyweb.org",
       chainId: 246,
       accounts: [deployKey]
+    }
+   
       
     }
-  }
-};
+  };
